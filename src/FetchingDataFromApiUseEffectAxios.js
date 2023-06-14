7c53136e68bcc6a842d4859663794906928a4400
abhashkumar <abhashmaddi@hotmail>
2023-06-14 12:02:59 +0530

    understanding babel and fetching data using axios in useeffect,using loader and exception handling

--- src/FetchingDataFromApiUseEffectAxios.js
+++ src/FetchingDataFromApiUseEffectAxios.js
@@ -1,4 +1,6 @@
 /*
+https://www.robinwieruch.de/react-hooks-fetch-data/
+
 
 The effect hook runs when the component mounts but also when the component updates. Because we are setting the state after every data fetch, the component updates and the effect runs again. It fetches the data again and again. That's a bug and needs to be avoided. We only want to fetch data when the component mounts. That's why you can provide an empty array as second argument to the effect hook to avoid activating it on component updates but only for the mounting of the component.
 
@@ -46,3 +48,311 @@ function App() {
 }
 
 export default App;
+
+
+
+/*
+
+====> notice how we are passing [query] inside useEffect
+
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://hn.algolia.com/api/v1/search?query=${query}`,
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, [query]);
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default App;
+
+
+
+------------------------------------------------------------------------------------------
+
+===> in above example on every type query changes useeffects executes and api gets called, so we used a button here
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `http://hn.algolia.com/api/v1/search?query=${query}`,
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, [search]);
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
+
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+
+
+-------------------------------------------------------------------------------------------------------------
+=====> just replaced search with URL directly
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(url);
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+----------------------------------------------------------------------------------------------------
+=====> using loading feature, showing loading until you have the data, also see how we are doing conditional rendering 
+
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+  );
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const result = await axios(url);
+
+      setData(result.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
+  );
+}
+
+export default App;
+
+-----------------------------------------------------------------------------------------------------------------
+
+====> Exceptional handline 
+
+
+
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
+  );
+}
+
+export default App;
+
+-----------------------------------------------------------------------------------------------------------------
+
+What about a proper form to fetch data? So far, we have only a combination of input field and button. Once you introduce more input elements, you may want to wrap them with a form element. In addition, a form makes it possible to trigger the button with "Enter" on the keyboard too.
+
+====> look at event.preventDefault, since clicking submit or pressing enter submits the form snd tries to reload the browser this line will prevent it 
+
+
+function App() {
+  ...
+
+  return (
+    <Fragment>
+      <form onSubmit={event => {
+        setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`);
+
+        event.preventDefault();
+      }}>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      ...
+    </Fragment>
+  );
+}
+
+
+
+
+
+*/
